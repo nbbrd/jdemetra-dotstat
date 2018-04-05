770cb1b3cc5b887561bfce6747e46722d9b421e1
Philippe Charles <philippe.charles@nbb.be>
2018-04-05 11:58:37 +0200

    Added ES source.

---
+++ sdmx-facade/sdmx-facade-web/src/main/java/internal/connectors/drivers/EsDriver.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2015 National Bank of Belgium
+ * 
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
+ * by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and 
+ * limitations under the Licence.
+ */
+package internal.connectors.drivers;
+
+import be.nbb.sdmx.facade.util.HasCache;
+import org.openide.util.lookup.ServiceProvider;
+import be.nbb.sdmx.facade.web.spi.SdmxWebDriver;
+import internal.connectors.ConnectorRestClient;
+import internal.web.SdmxWebDriverSupport;
+import it.bancaditalia.oss.sdmx.client.custom.DotStat;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ *
+ * @author Philippe Charles
+ */
+@ServiceProvider(service = SdmxWebDriver.class)
+public final class EsDriver implements SdmxWebDriver, HasCache {
+
+    @lombok.experimental.Delegate
+    private final SdmxWebDriverSupport support = SdmxWebDriverSupport
+            .builder()
+            .name("es@connectors")
+            .client(ConnectorRestClient.of(EsClient::new))
+            .supportedProperties(ConnectorRestClient.CONNECTION_PROPERTIES)
+            .sourceOf("ES", "Statistics Estonia", "http://andmebaas.stat.ee/restsdmx/sdmx.ashx")
+            .build();
+
+    private static final class EsClient extends DotStat {
+
+        public EsClient() throws URISyntaxException {
+            super("ES", new URI("http://andmebaas.stat.ee/restsdmx/sdmx.ashx"), false);
+        }
+    }
+}
---
+++ sdmx-facade/sdmx-facade-web/src/main/java/internal/web/drivers/DotStatDriver2.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017 National Bank of Belgium
+ * 
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
+ * by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and 
+ * limitations under the Licence.
+ */
+package internal.web.drivers;
+
+import be.nbb.sdmx.facade.LanguagePriorityList;
+import be.nbb.sdmx.facade.web.SdmxWebSource;
+import be.nbb.sdmx.facade.web.spi.SdmxWebDriver;
+import internal.web.SdmxWebDriverSupport;
+import internal.web.SdmxWebClient;
+import be.nbb.sdmx.facade.web.spi.SdmxWebContext;
+
+/**
+ *
+ * @author Philippe Charles
+ */
+public final class DotStatDriver2 implements SdmxWebDriver {
+
+    @lombok.experimental.Delegate
+    private final SdmxWebDriverSupport support = SdmxWebDriverSupport
+            .builder()
+            .name("dotstat@facade")
+            .client(DotStatDriver2::of)
+            .supportedProperties(Util.CONNECTION_PROPERTIES)
+            .sourceOf("OECD", "The Organisation for Economic Co-operation and Development", "https://stats.oecd.org/restsdmx/sdmx.ashx")
+            .sourceOf("ES", "Statistics Estonia", "http://andmebaas.stat.ee/restsdmx/sdmx.ashx")
+            .build();
+
+    private static SdmxWebClient of(SdmxWebSource s, LanguagePriorityList l, SdmxWebContext c) {
+        return new AbstractDotStat(s.getEndpoint(), l, Util.getRestClient(s, c));
+    }
+}
